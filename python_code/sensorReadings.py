f143b8442ed6ffd9e7a48c3e05c66c2316997be6
Jordan DuBose <dubosejordan13@gmail.com>
2026-03-05 17:42:15 -0500

    Update sensorReadings.py

--- python_code/sensorReadings.py
+++ python_code/sensorReadings.py
@@ -1,20 +1,16 @@
-import time
 import board
 import adafruit_sht4x
 
 # Initialize I2C
 i2c = board.I2C()
-
 # Initialize the SHT45 sensor
 sht = adafruit_sht4x.SHT4x(i2c)
 
 print("Found SHT4x with serial number", hex(sht.serial_number))
 sht.mode = adafruit_sht4x.Mode.NOHEAT_HIGHPRECISION
 
-#print out temp/humid vals
-while True:
-  temperature, humidity = sht.measurements
-  print("Temperature: %0.1f C" % temperature)
-  print("Humidity: %0.1f %%" % humidity)
-  print("")
-  time.sleep(1)
+#function values --> print out temp/humid vals
+def values():
+    tempC, humidity = sht.measurements
+    tempF = (tempC *9 /5)+32
+    return tempF, humidity
